d68f56a00d12632652d6bc736355ba528ef9831f
Oluwajuwon Olutomisin <jolutomisin@gmail.com>
2022-03-13 00:38:39 +0100

    add mobile nav functionality

--- main.js
+++ main.js
@@ -1,18 +1,22 @@
 //moebile menu navigation
 const openNav = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu-container');
-const closeNav = document.querySelector('.menu-btn i');
+const closeNav = document.querySelector('.menu-btn i')
+const menuBg = document.querySelector('.menu-bg');
 const navLink = document.querySelectorAll('.nav-link');
 
-openNav.addEventListener('click', e => {
+openNav.addEventListener('click', () => {
     menu.classList.toggle('active');
     closeNav.classList.toggle('active');
 });
+menuBg.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
 
 navLink.forEach(link => {
     link.addEventListener('click', () => {
         menu.classList.remove('active');
-        closeNav.classList.remove('active');
     })
 })
 
